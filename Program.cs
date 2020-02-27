f98fb42acb08325fb6260fb016afc9945d4b1919
charles-cohn <cohncl@gmatc.matc.edu>
2020-02-26 20:28:26 -0600

    Final Commit

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace genericsExample
+{     
+    public class Compare<T> 
+    {
+        public static bool AreEqual(T Value1, T Value2)
+        {
+            // Compare values
+            return Value1.Equals(Value2);
+        }
+    }
+
+    class Program
+    {        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This program compares two positive or negative integer values using generics\n");
+
+            Console.WriteLine("Enter a value");
+            int input1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter another value");
+
+            // string compare
+            // string input2 = Console.ReadLine();
+            // bool Equal = Compare<string>.AreEqual(input1, input2);
+
+            // integer compare
+            int input2 = Convert.ToInt32(Console.ReadLine());            
+            bool Equal = Compare<int>.AreEqual(input1, input2);
+
+            if (Equal)
+            {
+                Console.WriteLine("\nEqual");
+            }
+            else 
+            {
+
+                Console.WriteLine("\nNot Equal");
+            }
+
+            Console.WriteLine("\nPress any key to exit");
+            Console.ReadKey();
+        }
+    }
+}
